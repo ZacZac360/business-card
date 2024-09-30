cb068d2ac10695b7ab5fd932e640f0d162e79858
zai <zacgames.tv@gmail.com>
2024-09-30 15:22:16 +0800

    business

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,96 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+              backgroundImage: AssetImage('lib/images/img.png'),
+              radius: 60.0,
+            ),
+              Text(
+                'Zyrus Crispino',
+                style: TextStyle(
+                  fontFamily: 'ChakraPetch',
+                  fontSize: 35.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                  'App Developer',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w200,
+                    color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                width: 150.0,
+                child: Divider(
+                  thickness: 1.0,
+                  color: Colors.white
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                padding: EdgeInsets.all(20.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                      size: 20.0,
+                    ),
+                    SizedBox(
+                      width: 40.0,
+                    ),
+                    Text(
+                      '+63 929 381 1134'
+                    )
+                  ]
+                ),
+
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                padding: EdgeInsets.all(20.0),
+                color: Colors.white,
+                child: Row(
+                    children: [
+                      Icon(
+                        Icons.mail,
+                        color: Colors.teal,
+                        size: 20.0,
+                      ),
+                      SizedBox(
+                        width: 40.0,
+                      ),
+                      Text(
+                          'crispino.zyrus@gmail.com'
+                      )
+                    ]
+                ),
+
+              ),
+          ],
+        ),
       ),
     ),
-  );
+    );
+  }
 }
+
